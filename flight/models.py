dd51f0e915f8f54d758cc22ffa3bf9d117a4937b
Stanislav Nikulytsia <nikulicastas2004@gmail.com>
2023-09-07 17:32:44 -0500

    Add flight model

--- flight/models.py
+++ flight/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+from airplane.models import Airplane
 from airport.models import Airport
 
 
@@ -7,3 +8,10 @@ class Route(models.Model):
     source = models.ForeignKey(Airport, on_delete=models.CASCADE)
     destination = models.ForeignKey(Airport, on_delete=models.CASCADE)
     distance = models.IntegerField
+
+
+class Flight(models.Model):
+    route = models.ForeignKey(Route, on_delete=models.CASCADE)
+    airplane = models.ForeignKey(Airplane, on_delete=models.CASCADE)
+    departure_time = models.DateTimeField
+    arrival_time = models.DateTimeField
