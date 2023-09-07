352cd832cf2c21c5311bdfa4d8fd44fb39bafa5c
Stanislav Nikulytsia <nikulicastas2004@gmail.com>
2023-09-07 18:16:17 -0500

    Add migrations

--- airport/views.py
+++ airport/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from airport.models import Airport
+
+
+# class AirportViewSet(ModelViewSet):
+#     queryset = Airport.objects.all()
+#     serializer_class =
