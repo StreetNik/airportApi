b00b30b8a6e6195b3df42c7799086e8e1b08a745
Stanislav Nikulytsia <nikulicastas2004@gmail.com>
2023-09-07 17:39:13 -0500

    Add order model

--- order/models.py
+++ order/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+
+class Order(models.Model):
+    created_at = models.DateTimeField
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
