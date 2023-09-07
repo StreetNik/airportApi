49dfad4cd3002e1a137eedcc4c1acc2d370e1146
Stanislav Nikulytsia <nikulicastas2004@gmail.com>
2023-09-07 17:42:14 -0500

    Add Ticket model

--- order/models.py
+++ order/models.py
@@ -1,7 +1,16 @@
 from django.db import models
 from django.contrib.auth import get_user_model
 
+from flight.models import Flight
+
 
 class Order(models.Model):
     created_at = models.DateTimeField
     user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+
+
+class Ticket(models.Model):
+    row = models.IntegerField
+    seat = models.IntegerField
+    flight = models.ForeignKey(Flight, on_delete=models.CASCADE)
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
