0b118c2d63f8a3e6d8305a58bd5ca11cb31864d8
Stanislav Nikulytsia <nikulicastas2004@gmail.com>
2023-09-07 17:30:47 -0500

    Add Route model

--- flight/models.py
+++ flight/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+from airport.models import Airport
+
+
+class Route(models.Model):
+    source = models.ForeignKey(Airport, on_delete=models.CASCADE)
+    destination = models.ForeignKey(Airport, on_delete=models.CASCADE)
+    distance = models.IntegerField
