e155c480a4c1da9b40216301116fa717b17920c1
Stanislav Nikulytsia <nikulicastas2004@gmail.com>
2023-09-07 18:45:03 -0500

    Add viewset for airport endpoint

--- airport/views.py
+++ airport/views.py
@@ -1,8 +1,9 @@
 from rest_framework.viewsets import ModelViewSet
 
 from airport.models import Airport
+from airport.serializers import AirportSerializer
 
 
-# class AirportViewSet(ModelViewSet):
-#     queryset = Airport.objects.all()
-#     serializer_class =
+class AirportViewSet(ModelViewSet):
+    queryset = Airport.objects.all()
+    serializer_class = AirportSerializer
