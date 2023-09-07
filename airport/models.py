a655666ee7e36872da92fe3d37e8d76cb14e0f9a
Stanislav Nikulytsia <nikulicastas2004@gmail.com>
2023-09-07 17:13:04 -0500

    Add crew model

--- airport/models.py
+++ airport/models.py
@@ -4,3 +4,8 @@ from django.db import models
 class Airport(models.Model):
     name = models.CharField(max_length=255)
     closest_big_city = models.CharField(max_length=255)
+
+
+class Crew(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
