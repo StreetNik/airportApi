65999f150d69bedf98a92edc73f8a13f3686f0f3
Stanislav Nikulytsia <nikulicastas2004@gmail.com>
2023-09-09 10:52:36 -0500

    Add ordering to Crew and Airport models

--- airport/models.py
+++ airport/models.py
@@ -5,7 +5,19 @@ class Airport(models.Model):
     name = models.CharField(max_length=255)
     closest_big_city = models.CharField(max_length=255)
 
+    class Meta:
+        ordering = "closest_big_city"
+
+    def __str__(self):
+        return self.name + " - " + self.closest_big_city
+
 
 class Crew(models.Model):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
+
+    class Meta:
+        ordering = ("first_name", "last_name")
+
+    def __str__(self):
+        return self.first_name + " " + self.last_name
