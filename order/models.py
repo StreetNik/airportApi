97b1a91e841afce6d30a48dd39b178ea678376ea
Stanislav Nikulytsia <nikulicastas2004@gmail.com>
2023-09-09 11:46:52 -0500

    Add seats validation for ticket model

--- order/models.py
+++ order/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth import get_user_model
+from django.core.exceptions import ValidationError
 
 from flight.models import Flight
 
@@ -16,13 +17,20 @@ class Order(models.Model):
 
 
 class Ticket(models.Model):
-    row = models.IntegerField
-    seat = models.IntegerField
+    row = models.IntegerField()
+    seat = models.IntegerField()
     flight = models.ForeignKey(Flight, on_delete=models.CASCADE)
     order = models.ForeignKey(Order, on_delete=models.CASCADE)
 
     class Meta:
         ordering = ("order",)
+        unique_together = ("row", "seat", "flight")
+
+    def clean(self):
+        if self.row < 1 or self.row > self.flight.airplane.rows:
+            raise ValidationError("Row number is not within the valid range for this plane.")
+        if self.seat < 1 or self.seat > self.flight.airplane.seats_in_row:
+            raise ValidationError("Seat number is not within the valid range for this plane.")
 
     def __str__(self):
         return self.flight
