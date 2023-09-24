61d0809ffee53b88737767f5ee0d9a22e86395fd
Stanislav Nikulytsia <nikulicastas2004@gmail.com>
2023-09-23 23:45:38 -0500

    Add filtering for airports by name and closest big city name, add filtering for crew by first name and last name

--- airport/views.py
+++ airport/views.py
@@ -7,14 +7,38 @@ from airport.serializers import AirportSerializer, CrewSerializer
 
 
 class AirportViewSet(ModelViewSet):
-    queryset = Airport.objects.all()
     serializer_class = AirportSerializer
     permission_classes = (IsSuperuserOrReadOnly,)
     authentication_classes = (JWTAuthentication,)
 
+    def get_queryset(self):
+        name = self.request.query_params.get("name")
+        city = self.request.query_params.get("city")
+
+        queryset = Airport.objects.all()
+
+        if name:
+            queryset = queryset.filter(name__icontains=name)
+        if city:
+            queryset = queryset.filter(closest_big_city__icontains=city)
+
+        return queryset
+
 
 class CrewViewSet(ModelViewSet):
-    queryset = Crew.objects.all()
     serializer_class = CrewSerializer
     permission_classes = (IsSuperuserOrReadOnly,)
     authentication_classes = (JWTAuthentication,)
+
+    def get_queryset(self):
+        first_name = self.request.query_params.get("first_name")
+        last_name = self.request.query_params.get("last_name")
+
+        queryset = Crew.objects.all()
+
+        if first_name:
+            queryset = queryset.filter(first_name__icontains=first_name)
+        if last_name:
+            queryset = queryset.filter(last_name__icontains=last_name)
+
+        return queryset
