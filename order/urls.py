916e0d06a2e21aa35087baac1612811fcfa4f2c6
Stanislav Nikulytsia <nikulicastas2004@gmail.com>
2023-09-23 23:40:40 -0500

    Remove ticket endpoint

--- order/urls.py
+++ order/urls.py
@@ -1,11 +1,10 @@
 from rest_framework import routers
 
-from order.views import OrderViewSet, TicketViewSet
+from order.views import OrderViewSet
 
 router = routers.DefaultRouter()
 
 router.register(r"orders-list", OrderViewSet, basename="airport-detail")
-router.register(r"tickets-list", TicketViewSet, basename="ticket-detail")
 
 urlpatterns = [] + router.urls
 
