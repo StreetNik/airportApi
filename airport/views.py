db548c1c96034209d61f3d546bd75270073698ee
Stanislav Nikulytsia <nikulicastas2004@gmail.com>
2023-09-23 21:58:27 -0500

    Add restrictions for unauthenticated users(Can only do a GET request)

--- airport/views.py
+++ airport/views.py
@@ -1,5 +1,7 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework_simplejwt.authentication import JWTAuthentication
 
+from AirportAPI.permissions import IsSuperuserOrReadOnly
 from airport.models import Airport, Crew
 from airport.serializers import AirportSerializer, CrewSerializer
 
@@ -7,8 +9,12 @@ from airport.serializers import AirportSerializer, CrewSerializer
 class AirportViewSet(ModelViewSet):
     queryset = Airport.objects.all()
     serializer_class = AirportSerializer
+    permission_classes = (IsSuperuserOrReadOnly,)
+    authentication_classes = (JWTAuthentication,)
 
 
 class CrewViewSet(ModelViewSet):
     queryset = Crew.objects.all()
     serializer_class = CrewSerializer
+    permission_classes = (IsSuperuserOrReadOnly,)
+    authentication_classes = (JWTAuthentication,)
--- order/views.py
+++ order/views.py
@@ -3,12 +3,14 @@ from rest_framework.viewsets import ModelViewSet
 from order.models import Order, Ticket
 from order.serializers import OrderSerializer, TicketSerializer
 from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework.permissions import IsAuthenticated
 
 
 class OrderViewSet(ModelViewSet):
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
     authentication_classes = (JWTAuthentication,)
+    permission_classes = (IsAuthenticated,)
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
@@ -17,3 +19,5 @@ class OrderViewSet(ModelViewSet):
 class TicketViewSet(ModelViewSet):
     queryset = Ticket.objects.all()
     serializer_class = TicketSerializer
+    authentication_classes = (JWTAuthentication,)
+    permission_classes = (IsAuthenticated,)
