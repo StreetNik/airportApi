6323a52718971554e5d405330bae9a469e032d6c
Stanislav Nikulytsia <nikulicastas2004@gmail.com>
2023-09-23 23:46:32 -0500

    Add filtering for Flights by souce airport name, destination airport name and in date range

--- flight/views.py
+++ flight/views.py
@@ -7,16 +7,44 @@ from flight.serializers import RouteSerializer, FlightSerializer
 
 
 class FlightViewSet(ModelViewSet):
-    queryset = Flight.objects.all()
-
     permission_classes = (IsSuperuserOrReadOnly,)
     authentication_classes = (JWTAuthentication,)
     serializer_class = FlightSerializer
 
+    def get_queryset(self):
+        source = self.request.query_params.get("source")
+        destination = self.request.query_params.get("destination")
+        date_range_start = self.request.query_params.get("date_range_start")
+        date_range_end = self.request.query_params.get("date_range_end")
+
+        queryset = Flight.objects.all()
+
+        if source:
+            queryset = queryset.filter(route__source__name__icontains=source)
+        if destination:
+            queryset = queryset.filter(route__destination__name__icontains=destination)
+        if date_range_start:
+            queryset = queryset.filter(departure_time__gt=date_range_start)
+        if date_range_end:
+            queryset = queryset.filter(departure_time__lt=date_range_end)
+
+        return queryset
+
 
 class RouteViewSet(ModelViewSet):
-    queryset = Route.objects.all()
-
     permission_classes = (IsSuperuserOrReadOnly,)
     authentication_classes = (JWTAuthentication,)
     serializer_class = RouteSerializer
+
+    def get_queryset(self):
+        source = self.request.query_params.get("source")
+        destination = self.request.query_params.get("destination")
+
+        queryset = Route.objects.all()
+
+        if source:
+            queryset = queryset.filter(source__name__icontains=source)
+        if destination:
+            queryset = queryset.filter(destination__name__icontains=destination)
+
+        return queryset
