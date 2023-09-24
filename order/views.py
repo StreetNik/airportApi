0f67e94a2fa7f68b3c3a7481ec5e4e1aa380a225
Stanislav Nikulytsia <nikulicastas2004@gmail.com>
2023-09-23 23:43:20 -0500

    Update order viewset(now you can only do post and get requests), add filtering in date range, fix get_queryset method(now non-superuser can see only his orders)

--- order/views.py
+++ order/views.py
@@ -1,23 +1,43 @@
-from rest_framework.viewsets import ModelViewSet
+from rest_framework.viewsets import GenericViewSet
+from rest_framework import mixins
 
 from order.models import Order, Ticket
-from order.serializers import OrderSerializer, TicketSerializer
+from order.serializers import OrderSerializer, TicketSerializer, OrderListSerializer
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework.permissions import IsAuthenticated
 
 
-class OrderViewSet(ModelViewSet):
-    queryset = Order.objects.all()
+class OrderViewSet(
+    mixins.ListModelMixin,
+    mixins.CreateModelMixin,
+    GenericViewSet,
+):
     serializer_class = OrderSerializer
     authentication_classes = (JWTAuthentication,)
     permission_classes = (IsAuthenticated,)
 
+    def get_queryset(self):
+        user = self.request.user
+        date_range_start = self.request.query_params.get("date_range_start")
+        date_range_end = self.request.query_params.get("date_range_end")
+
+        queryset = Order.objects.all()
+
+        if not user.is_superuser:
+            queryset = queryset.filter(user=user)
+
+        if date_range_start:
+            queryset = queryset.filter(created_at__gt=date_range_start)
+        if date_range_end:
+            queryset = queryset.filter(created_at__lt=date_range_end)
+
+        return queryset
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return OrderListSerializer
+
+        return OrderSerializer
+
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
-
-
-class TicketViewSet(ModelViewSet):
-    queryset = Ticket.objects.all()
-    serializer_class = TicketSerializer
-    authentication_classes = (JWTAuthentication,)
-    permission_classes = (IsAuthenticated,)
