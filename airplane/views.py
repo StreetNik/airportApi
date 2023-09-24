1d9b5b202fb4cc2158fb12f44a8b4fe5b211c4b5
Stanislav Nikulytsia <nikulicastas2004@gmail.com>
2023-09-23 23:44:25 -0500

    Add filtering for airplanes by name and type, and filtering airplane types by name

--- airplane/views.py
+++ airplane/views.py
@@ -1,14 +1,42 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework_simplejwt.authentication import JWTAuthentication
 
+from AirportAPI.permissions import IsSuperuserOrReadOnly
 from airplane.models import Airplane, AirplaneType
 from airplane.serializers import AirplaneSerializer, AirplaneTypeSerializer
 
 
 class AirplaneViewSet(ModelViewSet):
-    queryset = Airplane.objects.all()
     serializer_class = AirplaneSerializer
+    permission_classes = (IsSuperuserOrReadOnly,)
+    authentication_classes = (JWTAuthentication,)
+
+    def get_queryset(self):
+        name = self.request.query_params.get("name")
+        type = self.request.query_params.get("type")
+
+        queryset = Airplane.objects.all()
+
+        if name:
+            queryset = queryset.filter(name__icontains=name)
+        if type:
+            queryset = queryset.filter(airplane_type__name=type)
+
+        return queryset.distinct()
 
 
 class AirplaneTypeViewSet(ModelViewSet):
-    queryset = AirplaneType.objects.all()
     serializer_class = AirplaneTypeSerializer
+    permission_classes = (IsSuperuserOrReadOnly,)
+    authentication_classes = (JWTAuthentication,)
+
+    def get_queryset(self):
+        name = self.request.query_params.get("name")
+
+        queryset = AirplaneType.objects.all()
+
+        if name:
+            queryset = queryset.filter(name=name)
+
+        return queryset
+
