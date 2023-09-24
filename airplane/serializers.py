5f0dbe54af652cb7dcc43da93c199ec304e4906d
Stanislav Nikulytsia <nikulicastas2004@gmail.com>
2023-09-23 23:36:50 -0500

    Update airplane type serializer(Add airplane type name field and make airplane field write only)

--- airplane/serializers.py
+++ airplane/serializers.py
@@ -10,7 +10,13 @@ class AirplaneTypeSerializer(serializers.ModelSerializer):
 
 
 class AirplaneSerializer(serializers.ModelSerializer):
+    airplane_type_name = serializers.CharField(source="airplane_type.name", read_only=True)
 
     class Meta:
         model = Airplane
-        fields = ["id", "name", "rows", "seats_in_row", "airplane_type"]
+        fields = [
+            "id", "name", "rows",
+            "seats_in_row", "airplane_type",
+            "airplane_type_name"
+        ]
+        extra_kwargs = {"airplane_type": {"write_only": True}}
