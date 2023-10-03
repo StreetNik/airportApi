6b153c9dc5f7f06544a54b5004afb9f467c2479c
Stanislav Nikulytsia <nikulicastas2004@gmail.com>
2023-10-02 20:46:50 -0500

    Add crew occupation model to airport models, add occupation field to Crew model, add experience_years field to Crew models

--- airport/models.py
+++ airport/models.py
@@ -12,12 +12,21 @@ class Airport(models.Model):
         return self.name + " - " + self.closest_big_city
 
 
+class CrewOccupation(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
 class Crew(models.Model):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
+    occupation = models.ForeignKey(CrewOccupation, on_delete=models.CASCADE)
+    experience_years = models.DecimalField(max_digits=4, decimal_places=2, default=0)
 
     class Meta:
         ordering = ("first_name", "last_name",)
 
     def __str__(self):
-        return self.first_name + " " + self.last_name
+        return self.first_name + " " + self.last_name + self.occupation.name
