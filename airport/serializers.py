55ecd4c20706a590c8a2d315011c46c8ab543621
Stanislav Nikulytsia <nikulicastas2004@gmail.com>
2023-10-02 20:47:49 -0500

    Add CrewOccupation serializer, Add role field to Crew serializer

--- airport/serializers.py
+++ airport/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from airport.models import Airport, Crew
+from airport.models import Airport, Crew, CrewOccupation
 
 
 class AirportSerializer(serializers.ModelSerializer):
@@ -10,6 +10,16 @@ class AirportSerializer(serializers.ModelSerializer):
 
 
 class CrewSerializer(serializers.ModelSerializer):
+    role = serializers.CharField(source="occupation.name", read_only=True)
     class Meta:
         model = Crew
-        fields = ["id", "first_name", "last_name"]
+        fields = ["id", "first_name", "last_name", "occupation", "role", "experience_years"]
+        extra_kwargs = {
+            "occupation": {"write_only": True},
+        }
+
+
+class CrewOccupationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CrewOccupation
+        fields = ["id", "name"]
